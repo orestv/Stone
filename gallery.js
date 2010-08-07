3c201c944b574b9b1207ac1bd01abe93c2362502
Orest Voloshchuk <orest.v@gmail.com>
2010-08-07 21:19:01 +0300

    Added previews for gallery

--- gallery.js
+++ gallery.js
@@ -1,10 +1,40 @@
-function clicked(sender){
-    var el = sender;
-    var x = 0, y = 0;
-    while (el != null){
-        y += el.offsetTop;
-        x += el.offsetLeft;
-        el = el.offsetParent;
+function show(id)
+{
+    document.getElementById('greyBackground').style.visibility='visible';
+    document.getElementById('largeView').style.visibility='visible';
+    document.getElementById('imgView').src = generateSampleString(id);
+}
+
+function hide()
+{
+    document.getElementById('greyBackground').style.visibility='hidden';
+    document.getElementById('largeView').style.visibility='hidden';
+    //document.getElementById('imgView').src = generateSampleString(id);
+}
+
+function generateList()
+{
+    var strUrl, div, img, centerDoc;
+    centerDoc = document.getElementById('centerDoc');
+    for (var i = 1; i < 36; i++)
+    {
+        div = document.createElement('div');
+        img = document.createElement('img');
+        div.setAttribute('class', 'galleryItem');
+        div.setAttribute('id', String(i));
+        div.setAttribute('onclick', 'show(' + String(i) + ');');
+
+        img.setAttribute('class', 'galleryImage');
+        img.setAttribute('src', generateSampleString(i));
+
+        div.appendChild(img);
+
+        centerDoc.appendChild(div);
+
     }
-    sender.style.left = 0;
+}
+
+function generateSampleString(id)
+{
+    return 'stone_colors/sample ' + String(id) + '.jpg';
 }

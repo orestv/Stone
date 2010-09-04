5c7eb4af8e1f4fc1a5942c501ccfe2af96ecb159
Orest Voloshchuk <orest.v@gmail.com>
2010-09-05 01:01:48 +0300

    Fixed up menu, added logo, buttons, ...

--- navigation.php
+++ navigation.php
@@ -1,6 +1,6 @@
 <?php
 
-$strMenuItem = '<a href="%1$s.php">%2$s</a>';
+$strMenuItem = '<td class="buttonBorder tdLeft"></td><td class="tdCenter"><a href="%1$s.php">%2$s</a></td><td class="buttonBorder tdRight"></td><td></td>';
 
 $arrMenu = array('index' => 'Головна',
     'examples' => 'Галерея виробів',
@@ -16,14 +16,19 @@ $arrMenu = array('index' => 'Головна',
     <div id="mainNavigationLogo">
     </div>
     <div id="mainNavigationMenu">
+        <table class="menuItem"><tr>
 
         <?php
         foreach ($arrMenu as $id => $name){
             printf($strMenuItem, $id, $name);
         }
-
-    ?>
+        ?>
+        </tr></table>
     </div> 
+    <hr style="width: 100%;border-bottom: 1px double #FF9F00;"/>
+    <div id="mainNavigationMotto">
+        Супер. Пафосний. Девіз.
+    </div>
 </div>
 
 <div id="mainNavigationContacts">

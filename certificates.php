081a65a3526ac30d9b6cd0df1e72d4116b9d50d0
Orest Voloshchuk <orest.v@gmail.com>
2010-09-11 14:28:23 +0300

    Added certificates page

---
+++ certificates.php
@@ -0,0 +1,21 @@
+<html>
+
+    <head>
+
+        <title>Сертифікати</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link href="style.css" rel="stylesheet" type="text/css"/>
+        <link href="navigation.css" rel="stylesheet" type="text/css"/>
+        <script type="text/javascript" src="func.js"></script>
+    </head>
+
+    <body>
+<?php include('navigation.php');?>
+        <div id="centerDoc">
+<h2>Сертифікати, якими засвідчується висока якість продукції <span class="term">Аспис-омега</span></h2>
+<a href="images/cert1.png"><img src="images/cert1.png" width="200"/></a>
+<a href="images/cert2.png"><img src="images/cert2.png" width="200"/></a>
+        </div>
+    </body>
+
+</html>

3ca7125d02e5108f9c5ff0afacfa4fdec05c44f2
Orest Voloshchuk <orest.v@gmail.com>
2010-09-11 18:23:35 +0300

    Added shapes page

---
+++ shapes.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2//EN">
+<html>
+<head>
+
+<title>Форми бортиків та торців</title>
+<meta name="generator" content=
+"HTML Tidy for Linux/x86 (vers 25 March 2009), see www.w3.org">
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link href="style.css" rel="stylesheet" type="text/css"/>
+<link href="navigation.css" rel="stylesheet" type="text/css"/>
+<script type="text/javascript" src="func.js"></script>
+</head>
+<body>
+<?php include('navigation.php');?>
+<div id="centerDoc">
+    <h2>Форми бортиків та торців</h2>
+        <p>За бажанням замовника вироби із штучного каменю (стільниці, підвіконня, барні стійки) можуть мати різноманітну конфігурацію не лише за формою, а й за виконанням бортиків та торцевих кромок.</p>
+    <h3>Форми бортиків:</h3>
+    <table>
+        <tr>
+            <td><img src="shapes/1_1.jpg"></img></td>
+            <td>із нижнім та верхнім заокругленням. H<sub>b</sub> = 10-70 мм</td>
+        </tr>
+        <tr>
+            <td><img src="shapes/1_2.jpg"></img></td>
+            <td>із верхнім заокругленням. H<sub>b</sub> = 10-70 мм</td>
+        </tr>
+    </table>
+    <h3>Форми торців:</h3>
+    <table>
+        <tr>
+            <td><img src="shapes/2_1.jpg"></img></td>
+            <td>із заокругленням верхнього кута радіусом 3-12,3 мм</td>
+        </tr>
+        <tr>
+            <td><img src="shapes/2_2.jpg"></img></td>
+            <td>із заокругленням верхнього кута та нижнім бортиком 10 мм</td>
+        </tr>
+        <tr>
+            <td><img src="shapes/2_3.jpg"></img></td>
+            <td>із заокругленням верхнього та нижнього кута кромки радіусом 3-12,3 мм</td>
+        </tr>
+        <tr>
+            <td><img src="shapes/2_4.jpg"></img></td>
+            <td>із фігурною фаскою</td>
+        </tr>
+        <tr>
+            <td><img src="shapes/2_5.jpg"></img></td>
+            <td>із округлою кромкою, R = 20 або 25 мм</td>
+        </tr>
+        <tr>
+            <td><img src="shapes/2_6.jpg"></img></td>
+            <td>прямокутна кромка</td>
+        </tr>
+    </table>
+
+
+</div>
+</body>
+</html>
